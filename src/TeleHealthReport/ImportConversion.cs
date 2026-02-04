6a93eb00008cf19dce45918a13fbb5deaef8f7b4
Chris Banwarth <github@aprettycoolprogram.com>
2026-02-04 14:21:57 -0500

    R26.2-development+041421

---
+++ src/TeleHealthReport/ImportConversion.cs
@@ -0,0 +1,17 @@
+﻿// 260204_code
+// 260204_documentation
+
+using System.Configuration;
+
+namespace TingenTransmorger.TeleHealthReport;
+
+class ImportConversion
+{
+    internal static void Adder(Configuration config)
+    {
+
+
+
+    }
+
+}
